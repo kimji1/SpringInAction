4a066c4c2dbe07f2be70940e024edaf7f107e151
kimji1 <dev.kimji1@gmail.com>
2021-12-01 23:00:09 +0900

    Initial commit

---
+++ src/main/groovy/com/example/tacocloud/TacoCloudApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.tacocloud
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TacoCloudApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication, args)
+	}
+
+}
